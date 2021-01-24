6967a216bd41bddc59c26b568b06efcb1c899617
Александр Тетенков <tetenkov@tecon.ru>
2021-01-24 23:29:10 +0300

    Сделал 4

--- task2.py
+++ task2.py
@@ -1,3 +1,9 @@
+"""
+https://drive.google.com/file/d/1S89m85BEBWtPLzWD1tpxRhjX6I7VOjie/view?usp=sharing
+Посчитать четные и нечетные цифры введенного натурального числа. Например, если введено число 34560,
+в нем 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
+"""
+
 def count(e, o, num):
     if num == 0:
         pass
